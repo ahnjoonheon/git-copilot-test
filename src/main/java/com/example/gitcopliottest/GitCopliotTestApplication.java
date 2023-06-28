a5a4e720e7e566cbee5f03830b0fae72bc9b9e08
안준헌 <frozengrape@mz.co.kr>
2023-06-28 09:45:06 +0900

    git copilot test init

---
+++ src/main/java/com/example/gitcopliottest/GitCopliotTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcopliottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCopliotTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCopliotTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitcopliottest/GitCopliotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcopliottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCopliotTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
